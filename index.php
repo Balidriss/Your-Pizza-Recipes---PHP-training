97e5f735212eaff895c77c8e4bb2924d12afa322
Idriss Balijon <I.balijon@gmail.com>
2024-01-16 18:18:49 +0100

    added users

--- index.php
+++ index.php
@@ -3,33 +3,53 @@
 
 $lorem = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia voluptatibus corporis, soluta et ex quo? Corporis eligendi iure harum quos tempora odio repellat ipsum unde sequi, dolorem magni debitis nam.";
 
+$users =
+    [
+        [
+            'full_name' => 'Classic Andy',
+            'email' => 'classic.andy@jaymeyl.com',
+            'age' => 69,
+        ],
+        [
+            'full_name' => 'Fabrice Bricefa',
+            'email' => 'four.furieu@jaymeyl.com',
+            'age' => 34,
+        ],
+        [
+            'full_name' => 'Anna Conda',
+            'email' => 'anna.conda@jaymeyl.com',
+            'age' => 28,
+        ],
+    ];
+
 // Déclaration du tableau des recettes : nom , contenu, author, afficher.
-$recipes = [
+$recipes =
     [
-        'title' => 'Pizza Classic',
-        'content' => $lorem,
-        'author' => 'classic.andy@jaymeyl.com',
-        'isDisplayed' => true,
-    ],
-    [
-        'title' => 'Pizza Yolo',
-        'content' => $lorem,
-        'author' => 'four.furieu@jaymeyl.com',
-        'isDisplayed' => false,
-    ],
-    [
-        'title' => 'Pizza Spicy',
-        'content' => $lorem,
-        'author' => 'four.furieu@jaymeyl.com',
-        'isDisplayed' => true,
-    ],
-    [
-        'title' => 'Pizza Reptilien',
-        'content' => $lorem,
-        'author' => 'Anna.Conda@jaymeyl.com',
-        'isDisplayed' => true,
-    ],
-];
+        [
+            'title' => 'Pizza Classic',
+            'content' => $lorem,
+            'author' => 'classic.andy@jaymeyl.com',
+            'isDisplayed' => true,
+        ],
+        [
+            'title' => 'Pizza Yolo',
+            'content' => $lorem,
+            'author' => 'four.furieu@jaymeyl.com',
+            'isDisplayed' => false,
+        ],
+        [
+            'title' => 'Pizza Spicy',
+            'content' => $lorem,
+            'author' => 'four.furieu@jaymeyl.com',
+            'isDisplayed' => true,
+        ],
+        [
+            'title' => 'Pizza Reptilien',
+            'content' => $lorem,
+            'author' => 'anna.conda@jaymeyl.com',
+            'isDisplayed' => true,
+        ],
+    ];
 
 $totalrecipes = 4;
 
