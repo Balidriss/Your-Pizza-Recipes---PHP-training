c01de42cfb9d654471188defa33b367cb8529de0
Idriss Balijon <I.balijon@gmail.com>
2024-01-16 20:27:03 +0100

    added bootstrap

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <head>
     <title>Affichage des recettes de Pizza</title>
     <link href="style/style.css" type="text/css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
