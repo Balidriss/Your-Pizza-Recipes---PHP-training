6bf4d5a49099a60cc54073738a82e068bd792ea0
Idriss Balijon <I.balijon@gmail.com>
2024-01-16 20:50:53 +0100

    added container in recipe section

--- index.php
+++ index.php
@@ -17,30 +17,32 @@ require_once(__DIR__ . '/includes/function.php');
     <?php require_once(__DIR__ . '/includes/header.php'); ?>
 
     <section id="section-recipes">
-        <h1>Pizza Recipes</h1>
+        <div class="container">
+            <h1>Pizza Recipes</h1>
 
-        <?php
-        foreach (displayableRecipes($recipes) as $item) : ?>
+            <?php
+            foreach (displayableRecipes($recipes) as $item) : ?>
 
 
-            <article class='<?php echo strtolower(preg_replace('/\s+/', '-', $item['title'])); ?>'>
-                <h2>
-                    <?php echo $item['title']; ?>
-                </h2>
+                <article class='<?php echo strtolower(preg_replace('/\s+/', '-', $item['title'])); ?>'>
+                    <h2>
+                        <?php echo $item['title']; ?>
+                    </h2>
 
-                <p>
-                    <?php echo $item['content']; ?>
-                </p>
+                    <p>
+                        <?php echo $item['content']; ?>
+                    </p>
 
-                <p>from :
-                    <em> <?php echo authorInfo($item['author'], $users)['full_name']; ?> </em>
-                    <?php if (authorInfo($item['author'], $users)['age'] >= 0) : ?>
-                        - Age : <em> <?php echo authorInfo($item['author'], $users)['age'] ?> years old.</em>
-                    <?php endif; ?>
-                </p>
-            </article>
+                    <p>from :
+                        <em> <?php echo authorInfo($item['author'], $users)['full_name']; ?> </em>
+                        <?php if (authorInfo($item['author'], $users)['age'] >= 0) : ?>
+                            - Age : <em> <?php echo authorInfo($item['author'], $users)['age'] ?> years old.</em>
+                        <?php endif; ?>
+                    </p>
+                </article>
 
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </section>
     <?php require_once(__DIR__ . '/includes/footer.php'); ?>
 </body>
