9319d921c740918aafb85f9308eed10487371cc5
Idriss Balijon <I.balijon@gmail.com>
2024-01-16 20:30:53 +0100

    added content in header & footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,6 @@
-<?php
-echo 'footer here';
+<footer class="bg-light text-center text-lg-start mt-auto">
+    <div class="text-center p-3">
+        © 2021 Copyright:
+        <a class="text-dark" href="">Idriss Balijon</a>
+    </div>
+</footer>
