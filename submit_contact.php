a6d98792a0ee912734edd2cdd15f71fa37885830
Idriss Balijon <I.balijon@gmail.com>
2024-01-17 13:52:36 +0100

    added container

--- submit_contact.php
+++ submit_contact.php
@@ -23,14 +23,16 @@ if (
 
 <body>
     <?php require_once(__DIR__ . '/includes/header.php'); ?>
-    <h1>Message received !</h1>
+    <div class="container">
+        <h1>Message received !</h1>
 
-    <div class="card">
+        <div class="card">
 
-        <div class="card-body">
-            <h5 class="card-title">Review your information</h5>
-            <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
-            <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+            <div class="card-body">
+                <h5 class="card-title">Review your information</h5>
+                <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
+                <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+            </div>
         </div>
     </div>
     <?php require_once(__DIR__ . '/includes/footer.php'); ?>
