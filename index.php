de12a01da594321e0d703f4b7812725d38767cda
Idriss Balijon <I.balijon@gmail.com>
2024-01-17 17:56:45 +0100

    html form done

--- index.php
+++ index.php
@@ -53,11 +53,17 @@ require_once(__DIR__ . '/includes/function.php');
         <section id="section-login">
             <div class="container">
                 <h2>Login</h2>
-
                 <form action="page_login.php" method="post">
-                    <div class=" mb-3">
+                    <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
+                        <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" aria-describedby="password-help" required>
+                    </div>
+                    <div class=" mb-3">
+                        <button type="submit" class="btn btn-primary">Login</button>
                     </div>
                 </form>
             </div>
