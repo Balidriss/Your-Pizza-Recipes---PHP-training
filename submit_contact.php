6cc0c8505268c538ad8f5e55bbc5176da3cbe4d1
Idriss Balijon <I.balijon@gmail.com>
2024-01-17 13:25:48 +0100

    added includes and tags

--- submit_contact.php
+++ submit_contact.php
@@ -1,10 +1,32 @@
-<h1>Message received !</h1>
+<?php
+$getData = $_GET;
 
-<div class="card">
+if (!isset($getData['email']) || !isset($getData['message'])) {
+    echo ('Email and message are missing. Please make sure you completed the form correctly.');
 
-    <div class="card-body">
-        <h5 class="card-title">Review your information</h5>
-        <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
-        <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+    return;
+}
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Your Pizza ! - Home</title>
+    <link href="style/style.css" type="text/css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php require_once(__DIR__ . '/includes/header.php'); ?>
+    <h1>Message received !</h1>
+
+    <div class="card">
+
+        <div class="card-body">
+            <h5 class="card-title">Review your information</h5>
+            <p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?></p>
+            <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?></p>
+        </div>
     </div>
-</div>
+    <?php require_once(__DIR__ . '/includes/footer.php'); ?>
+</body>
