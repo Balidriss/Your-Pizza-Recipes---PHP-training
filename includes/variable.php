1ec865fb85fd0442956101338ccbde99c7416b00
Idriss Balijon <I.balijon@gmail.com>
2024-01-24 20:37:38 +0100

    linked to mysql, replaced static variable to user & recipe db

--- includes/variable.php
+++ includes/variable.php
@@ -1,59 +1,14 @@
 <?php
-$lorem = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia voluptatibus corporis, soluta et ex quo? Corporis eligendi iure harum quos tempora odio repellat ipsum unde sequi, dolorem magni debitis nam.";
+try {
+    $mysqlClient = new PDO('mysql:host=localhost;dbname=your_pizza;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
 
-$users =
-    [
-        [
-            'full_name' => 'Classic Andy',
-            'email' => 'classic.andy@jaymeyl.com',
-            'age' => 69,
-            'isLoggedIn' => false,
-            'password' => 'CA0000',
-            'user_id' => ''
-        ],
-        [
-            'full_name' => 'Fabrice Bricefa',
-            'email' => 'four.furieu@jaymeyl.com',
-            'age' => 34,
-            'isLoggedIn' => false,
-            'password' => 'FB0000',
-            'user_id' => ''
-        ],
-        [
-            'full_name' => 'Anna Conda',
-            'email' => 'anna.conda@jaymeyl.com',
-            'age' => 28,
-            'isLoggedIn' => false,
-            'password' => 'FB0000',
-            'user_id' => ''
-        ],
-    ];
+$recipesStatement = $mysqlClient->prepare('SELECT * FROM recipes');
+$usersStatement = $mysqlClient->prepare('SELECT * FROM users');
 
-// Déclaration du tableau des recettes : nom , contenu, author, afficher.
-$recipes =
-    [
-        [
-            'title' => 'Pizza Classic',
-            'content' => $lorem,
-            'author' => 'classic.andy@jaymeyl.com',
-            'isDisplayed' => true,
-        ],
-        [
-            'title' => 'Pizza Yolo',
-            'content' => $lorem,
-            'author' => 'four.furieu@jaymeyl.com',
-            'isDisplayed' => true,
-        ],
-        [
-            'title' => 'Pizza Spicy',
-            'content' => $lorem,
-            'author' => 'four.furieu@jaymeyl.com',
-            'isDisplayed' => false,
-        ],
-        [
-            'title' => 'Pizza Reptilien',
-            'content' => $lorem,
-            'author' => 'anna.conda@jaymeyl.com',
-            'isDisplayed' => true,
-        ],
-    ];
+$recipesStatement->execute();
+$usersStatement->execute();
+$recipes = $recipesStatement->fetchAll();
+$users = $usersStatement->fetchAll();
