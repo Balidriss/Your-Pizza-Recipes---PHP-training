22210cb24ad16ad5fef07e1b223de53211360b1a
Idriss Balijon <I.balijon@gmail.com>
2024-01-25 10:53:58 +0100

    database connect and fetch files split

--- includes/variable.php
+++ includes/variable.php
@@ -1,9 +1,4 @@
 <?php
-try {
-    $mysqlClient = new PDO('mysql:host=localhost;dbname=your_pizza;charset=utf8', 'root', '');
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
 
 $recipesStatement = $mysqlClient->prepare('SELECT * FROM recipes WHERE is_enabled = TRUE');
 $usersStatement = $mysqlClient->prepare('SELECT * FROM users');
